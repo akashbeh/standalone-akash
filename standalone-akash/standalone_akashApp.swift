d477b0b19f9de5a24a7e35dbdea74c0072c113b8
--unset <--unset>
2024-03-18 18:02:17 -0700

    Initial Commit

---
+++ standalone-akash/standalone_akashApp.swift
@@ -0,0 +1,17 @@
+//
+//  standalone_akashApp.swift
+//  standalone-akash
+//
+//  Created by keckuser on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct standalone_akashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
