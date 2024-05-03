245d78d44b37023df7f394e7fa80a0cff6022469
akashbeh <112662680+akashbeh@users.noreply.github.com>
2024-05-03 16:07:22 -0700

    Removed unused code

--- standalone-akash/Model/ServerData.swift
+++ standalone-akash/Model/ServerData.swift
@@ -44,26 +44,3 @@ class ServerData: ObservableObject {
     }
     
 }
-
-
-func load<T: Decodable>(_ filename: String) -> T { // from Brian Bowers
-    let data: Data
-    
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
-    }
-    
-    do{
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldnt load \(filename) from main bundle: \n\(error)")
-    }
-    
-    do{
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch{
-        fatalError("Couldnt parse \(filename) as \(T.self):\n\(error)")
-    }
-}
